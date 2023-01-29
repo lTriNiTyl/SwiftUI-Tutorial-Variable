8687fd48e495205423c6e7b170d6084b37c91d4f
lTriNiTyl <antoniokr@gmail.com>
2023-01-29 17:06:26 +0900

    Initial Commit

---
+++ Variable/VariableApp.swift
@@ -0,0 +1,17 @@
+//
+//  VariableApp.swift
+//  Variable
+//
+//  Created by Hyung-Jin Kim on 2023/01/29.
+//
+
+import SwiftUI
+
+@main
+struct VariableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
